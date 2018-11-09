40d14ab913df1b1dc64930c50f36b171d914946b
Hashrocket Workstation <dev@hashrocket.com>
2018-11-09 15:40:20 -0600

    Fetch Google credentials from the environment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,8 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.secrets.GOOGLE_CLIENT_ID, Rails.application.secrets.GOOGLE_CLIENT_SECRET, {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, 
+    ENV.fetch("GOOGLE_CLIENT_ID")
+    ENV.fetch("GOOGLE_CLIENT_SECRET")
+    {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
